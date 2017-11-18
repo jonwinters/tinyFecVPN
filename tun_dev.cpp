7c323b45d5b12dbf8f7b7049e95f8b28e9b62f96
winters <winters@wintersdeiMac.local>
2017-11-18 21:09:32 +0800

    反阉割一刀

--- tun_dev.cpp
+++ tun_dev.cpp
@@ -138,13 +138,15 @@ int from_fec_to_normal2(conn_info_t & conn_info,dest_t &dest,char * data,int len
 			{
 				u32_t dest_ip=htonl(read_u32(tmp_data+16));
 				//printf("%s\n",my_ntoa(dest_ip));
-				if(  ( ntohl(sub_net_uint32)&0xFFFFFF00 ) !=  ( ntohl (dest_ip) &0xFFFFFF00) )
+				/** 关闭过滤
+				 * if(  ( ntohl(sub_net_uint32)&0xFFFFFF00 ) !=  ( ntohl (dest_ip) &0xFFFFFF00) )
 				{
 					string sub=my_ntoa(dest_ip);
 					string dst=my_ntoa( htonl( ntohl (sub_net_uint32) &0xFFFFFF00)   );
 					mylog(log_warn,"[restriction]packet's dest ip [%s] not in subnet [%s],dropped\n", sub.c_str(), dst.c_str());
 					continue;
 				}
+				**/
 			}
 		}
 #endif
